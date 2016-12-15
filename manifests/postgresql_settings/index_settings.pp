346558fd70779aea6b73c26f2285eb469bdd0a83
Nick Walker <nick.walker@puppetlabs.com>
2016-12-15 13:47:21 -0800

    Restrict indexes settings to 2016.4
    Prior to this commit, the index_settings would attempt to be
    applied on all versions of PE.  Since the index names may have
    changed or maybe the indexes didn't even exist in a past version
    this could cause problems.
    
    After this commit, the index_settings will only apply on 2016.4
    for now and future iterations could allow figuring out which
    indexes exist in different versions of PE and probably tuning them.

--- manifests/postgresql_settings/index_settings.pp
+++ manifests/postgresql_settings/index_settings.pp
@@ -1,27 +1,29 @@
 class pe_databases::postgresql_settings::index_settings {
 
-  $factsets_indexes = [ 'factsets_pkey',
-                        'factsets_certname_idx',
-                        'factsets_hash_expr_idx',
-                        'idx_factsets_prod', ]
+  if $::pe_server_version =~ /^2016.4./ {
 
-  pe_databases::set_puppetdb_index_fillfactor { $factsets_indexes :
-    fillfactor => 50,
+    $factsets_indexes = [ 'factsets_pkey',
+                          'factsets_certname_idx',
+                          'factsets_hash_expr_idx',
+                          'idx_factsets_prod', ]
+
+    pe_databases::set_puppetdb_index_fillfactor { $factsets_indexes :
+      fillfactor => 50,
+    }
+
+    $fact_values_indexes = [ 'fact_values_pkey',
+                             'fact_values_value_hash_key',
+                             'fact_values_value_float_idx',
+                             'fact_values_value_integer_idx', ]
+
+    pe_databases::set_puppetdb_index_fillfactor { $fact_values_indexes :
+      fillfactor => 50,
+    }
+
+    $edges_indexes = [ 'edges_certname_source_target_type_unique_key' ]
+
+    pe_databases::set_puppetdb_index_fillfactor { $edges_indexes :
+      fillfactor => 75,
+    }
   }
-
-  $fact_values_indexes = [ 'fact_values_pkey',
-                           'fact_values_value_hash_key',
-                           'fact_values_value_float_idx',
-                           'fact_values_value_integer_idx', ]
-
-  pe_databases::set_puppetdb_index_fillfactor { $fact_values_indexes :
-    fillfactor => 50,
-  }
-
-  $edges_indexes = [ 'edges_certname_source_target_type_unique_key' ]
-
-  pe_databases::set_puppetdb_index_fillfactor { $edges_indexes :
-    fillfactor => 75,
-  }
-
 }
